e370fba5eecd7625ee2ead5e2ff2d108499a9e86
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-12-02 22:15:23 +0100

    chmod on 0-gather_data_from_an_API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -24,8 +24,12 @@ if __name__ == "__main__":
         if i.get('userId') == int(argv[1]):
             total += 1
 
+            if i.get('completed') is True:
+                completed += 1
+                nb_task.append(i.get('title'))
+
     print("Employee {} is done with tasks({}/{}):".format(
         employee, completed, total))
 
-    for i in nb_task:
-        print("\t {}".format(i))
+    for nb_task in nb_task:
+        print("\t {}".format(nb_task))
