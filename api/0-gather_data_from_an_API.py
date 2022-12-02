f7bef369f4e9dca0b9b395cce83f3ed1b1a6decb
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-12-02 17:46:58 +0100

    modify scriptfile0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,26 +7,28 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    # First connect/open/read/loads to the data url users
-    url_first = get('https://jsonplaceholder.typicode.com/todos/')
-    data_url_first = url_first.json()
+    url_one = get('https://jsonplaceholder.typicode.com/todos/')
+    data_one = url_one.json()
     completed = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    NUMBER_OF_DONE_TASKS = []
+    total = 0
+    nb_task = []
     # Second connect/open/read/loads to the data url todos
-    url_second = get('https://jsonplaceholder.typicode.com/users/')
-    data_url_second = url_second.json()
-    for i in data_url_first:
-        if i.get('userId') == int(argv[1]):
-            TOTAL_NUMBER_OF_TASKS += 1
-            if i.get('completed') is True:
-                completed += 1
-                NUMBER_OF_DONE_TASKS.append(i.get('title'))
-    for i in data_url_second:
+    url_two = get('https://jsonplaceholder.typicode.com/users/')
+    data_two = url_two.json()
+
+    for i in data_two:
         if i.get('id') == int(argv[1]):
             employee = i.get('name')
-    # Third connect/open/read/loads to the data url todos
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee, completed, TOTAL_NUMBER_OF_TASKS))
-    for NUMBER_OF_DONE_TASKS in NUMBER_OF_DONE_TASKS:
-        print("\t {}".format(NUMBER_OF_DONE_TASKS))
+
+    for i in data_one:
+        if i.get('userId') == int(argv[1]):
+            completed = i.get('completed')
+            total = total + 1
+
+            if i.get('completed') is True:
+                completed = completed + 1
+                nb_task.append(i.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(employee,
+                                                          completed, total))
+    for i in nb_task:
+        print("\t {}".format(i))
