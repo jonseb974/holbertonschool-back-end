341e7534d438dd43f95e3472880244823dec3910
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-12-02 16:52:00 +0100

    modify import call file 0-gather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,7 +2,6 @@
 """Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import urllib
 from requests import get
 from sys import argv
 
