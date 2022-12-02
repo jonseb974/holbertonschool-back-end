3a6e8d3a37410d4c3fee55430087d781381d8cf6
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-12-02 18:37:45 +0100

    createfile1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,3 +2,30 @@
 """Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
+from requests import get
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    first = get('https://jsonplaceholder.typicode.com/todos/')
+    data_one = first.json()
+
+    row = []
+    second = get()
+    data_two = second.json('https://jsonplaceholder.typicode.com/users/')
+
+    for todo in data_two:
+        if todo['id'] == int(['id']):
+            employee = todo['username']
+
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        wrti = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        for todo in data_one:
+            row = []
+            if todo['userId'] == int(argv[1]):
+                row.append(employee)
+                row.append(todo['completed'])
+                row.append(todo['title'])
+                row.append(todo['userId'])
+                wrti.writerow(row)
