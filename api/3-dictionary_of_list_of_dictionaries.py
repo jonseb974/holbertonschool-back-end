3e7412cf36340ca3163392fde60dd371a49a2847
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-12-02 21:08:39 +0100

    create file3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format."""
+
+from requests import get
+import json
+
+if __name__ == '__main__':
+    url_one = get('https://jsonplaceholder.typicode.com/todos/')
+    data_one = url_one.json()
+
+    row = []
+    url_two = get('https://jsonplaceholder.typicode.com/users/')
+    data_two = url_two.json()
+
+    new_dict = {}
+
+    for j in data_two:
+        row = []
+        for i in data_one:
+            new_dict2 = {}
+            if j['id'] == i['userId']:
+                new_dict2['id'] = j['username']
+                new_dict2['name'] = i['title']
+                new_dict2['completed'] = i['completed']
+                row.append(new_dict2)
+        new_dict[j['id']] = row
+
+    with open("todo_all_employees.json", 'w') as file:
+        json_obj = json.dumps(new_dict)
+        file.write(json_obj)
