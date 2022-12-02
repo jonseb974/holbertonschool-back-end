d83f025a66767bcb4ea755d3936c920167f4e981
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-12-02 16:35:36 +0100

    createfirst script file 0-gather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""Script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import urllib
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    # First connect/open/read/loads to the data url users
+    url_first = get('https://jsonplaceholder.typicode.com/todos/')
+    data_url_first = url_first.json()
+    completed = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = []
+    # Second connect/open/read/loads to the data url todos
+    url_second = get('https://jsonplaceholder.typicode.com/users/')
+    data_url_second = url_second.json()
+    for i in data_url_first:
+        if i.get('userId') == int(argv[1]):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if i.get('completed') is True:
+                completed += 1
+                NUMBER_OF_DONE_TASKS.append(i.get('title'))
+    for i in data_url_second:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
+    # Third connect/open/read/loads to the data url todos
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee, completed, TOTAL_NUMBER_OF_TASKS))
+    for NUMBER_OF_DONE_TASKS in NUMBER_OF_DONE_TASKS:
+        print("\t {}".format(NUMBER_OF_DONE_TASKS))
